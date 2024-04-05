d838668ab819bd0f7f073306edb41d7345e56a7d
toyotarou <toyodahideyuki4819@gmail.com>
2024-04-05 22:26:02 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/test_cloud_functions/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_cloud_functions
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(home: MyHomePage(title: 'Flutter Demo Home Page'));
+}
+
+////////////////////////////////////
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 1;
+
+  final functions = FirebaseFunctions.instance;
+
+  Future<void> callFunction() async {
+    try {
+      final callable = functions.httpsCallable('doubleNumber');
+
+      final result = await callable.call({'number': _counter});
+
+      final data = result.data;
+
+      setState(() => _counter = data['result']);
+
+      print('結果: ${data['result']}');
+    } catch (e) {
+      print('エラー: $e'); // エラーハンドリング
+    }
+  }
+
+  ///
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
+          ],
+        ),
+      ),
+
+      ///
+      floatingActionButton: FloatingActionButton(onPressed: callFunction, tooltip: 'Increment', child: const Icon(Icons.add)),
+
+      ///
+    );
+  }
+}
